331efbd2c88c3b3ff4d66f861cd21297769bf127
Clément Bénier <clement.benier@iot.bzh>
2020-01-07 16:50:06 +0100

    AGL widget: cmake + config + prefix
    - config.xml.in
    - config.cmake
    - modbus-binding:
        - change prefix to control
        - binder name is modbus
    
    so config files should start with control-modbus*
    
    Change-Id: Ib7c5d0e75bfa3eb72e727879c9b466fb5642c01c
    Signed-off-by: Clément Bénier <clement.benier@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -139,7 +139,7 @@ set(WIDGET_TYPE application/vnd.agl.service)
 # This is the file that will be executed, loaded,
 # at launch time by the application framework.
 #
-set(WIDGET_ENTRY_POINT config.xml)
+set(WIDGET_ENTRY_POINT lib/afb-modbus.so)
 
 # Optional dependencies order
 # ---------------------------
